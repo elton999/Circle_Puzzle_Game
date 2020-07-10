68767bbdc067e4793cab54cc85f273aea0fe5335
Elton Silva <eltoncarsilva73@gmail.com>
2020-07-10 19:27:05 -0300

    level 9 sprites

---
+++ ball/Gameplay/Levels/Level_09/Level.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using tainicom.Aether.Physics2D.Dynamics;
+using UmbrellaToolKit;
+using UmbrellaToolKit.UI;
+
+namespace ball.Gameplay.Levels.Level_09
+{
+    public class Level : Stage
+    {
+        public override void Start(ContentManager Content, World World, MouseManager Mouse)
+        {
+            this.Content = Content;
+            this.World = World;
+            this.Mouse = Mouse;
+
+            this.ResetLevel();
+        }
+
+        public override void ResetLevel()
+        {
+
+        }
+
+        public override void Destroy()
+        {
+
+        }
+
+
+        public override void UpdateLevel(GameTime gameTime)
+        {
+            this.Update(gameTime);
+        }
+
+        public override void DrawLevel(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice)
+        {
+            this.Draw(spriteBatch, graphicsDevice);
+        }
+    }
+}
